685e420e9cbfbf6a891feb6d7e845d72cf1afbf2
JiHwan Eom <snakeblanket@snblk-macbookpro.tail248eb.ts.net>
2024-11-07 10:33:05 +0900

    feat: upload default structure

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { config } from 'dotenv';
+import logger from 'morgan';
+import helmet from 'helmet';
+
+config();
+const app = express();
+app.set('port', process.env.PORT || 3001);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(
+  cors({
+    origin: ['https://daon.today/'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }),
+);
+
+app.listen(app.get('port'), () => {
+  console.log(`🚀 ProjectDaon API is running on port ${app.get('port')}`);
+});
