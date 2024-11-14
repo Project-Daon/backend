928c20fbdaa4fd3cef5a56ecd505799fb451dc88
JiHwan Eom <snakeblanket@SNBLK-MacBookPro.local>
2024-11-14 23:14:49 +0900

    feat: diary get with specific date

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,10 @@ app.listen(app.get('port'), () => {
 });
 
 import indexRouter from './routes/index';
+import diaryRouter from './routes/diary';
 import naverRouter from './routes/oauth/naver';
 import kakaoRouter from './routes/oauth/kakao';
 app.use('/', indexRouter);
+app.use('/diary', diaryRouter);
 app.use('/login/naver', naverRouter);
 app.use('/login/kakao', kakaoRouter);
--- src/routes/diary.ts
+++ src/routes/diary.ts
@@ -33,6 +33,7 @@ type Table_Users = {
 type Table_Diary = {
   userid: string;
   date: string;
+  feel: number;
   content: string;
 };
 
@@ -113,8 +114,13 @@ router.post(
 
       // Insert into DIARY_DAILY_DATA if not exists else update
       await connection.execute(
-        'INSERT INTO DIARY_DAILY_DATA (userid, date, content) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE content = ?',
-        [req.userId, formattedDate, '', ''],
+        'INSERT INTO DIARY_DAILY_DATA (userid, date, feel, content) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE content = ?',
+        [
+          req.userId,
+          formattedDate,
+          req.body.feel as number,
+          req.body.content as string,
+        ],
       );
       const [diarys] = await connection.query(
         'SELECT * FROM DIARY_DAILY_DATA WHERE userid = ? AND date = ?',
@@ -139,53 +145,49 @@ router.post(
   },
 );
 
-//
-// router.get(
-//   '/sdate',
-//   authMiddleware,
-//   async (req: RequestWithUserId, res: Response) => {
-//     const connection = await pool.getConnection();
-//
-//     try {
-//       const [accounts] = await connection.query(
-//         'SELECT * FROM accounts WHERE id = ?',
-//         [req.userId],
-//       );
-//       const account: Table_Users = (accounts as Table_Users[])[0];
-//
-//       if (!account) {
-//         return res.status(500).json({
-//           code: 'NOT_FOUND',
-//           message: '유저를 찾을 수 없습니다',
-//         });
-//       }
-//
-//       const date = new Date();
-//       const year = date.getFullYear();
-//       const month = String(date.getMonth() + 1).padStart(2, '0');
-//       const day = String(date.getDate()).padStart(2, '0');
-//       const formattedDate = `${year}-${month}-${day}`;
-//
-//       const [diarys] = await connection.query(
-//         'SELECT * FROM DIARY_DAILY_DATA WHERE userid = ? AND date = ?',
-//         [req.userId, formattedDate],
-//       );
-//
-//       const diary: Table_Diary = (diarys as Table_Diary[])[0];
-//
-//       if (diary) {
-//         return res.status(200).json({
-//           status: 200,
-//           content: diary.content,
-//         });
-//       } else {
-//         return res.status(500).json({
-//           status: 500,
-//           msg: 'INTERNAL_SERVER_ERROR',
-//         });
-//       }
-//     } finally {
-//       connection.release();
-//     }
-//   },
-// );
+router.get(
+  '/sdate',
+  authMiddleware,
+  async (req: RequestWithUserId, res: Response) => {
+    const connection = await pool.getConnection();
+    const formattedDate = req.query.date as string;
+
+    try {
+      const [accounts] = await connection.query(
+        'SELECT * FROM accounts WHERE id = ?',
+        [req.userId],
+      );
+      const account: Table_Users = (accounts as Table_Users[])[0];
+
+      if (!account) {
+        return res.status(500).json({
+          code: 'NOT_FOUND',
+          message: '유저를 찾을 수 없습니다',
+        });
+      }
+
+      const [diarys] = await connection.query(
+        'SELECT * FROM DIARY_DAILY_DATA WHERE userid = ? AND date = ?',
+        [req.userId, formattedDate],
+      );
+
+      const diary: Table_Diary = (diarys as Table_Diary[])[0];
+
+      if (diary) {
+        return res.status(200).json({
+          status: 200,
+          content: diary.content,
+        });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          msg: 'INTERNAL_SERVER_ERROR',
+        });
+      }
+    } finally {
+      connection.release();
+    }
+  },
+);
+
+export default router;
