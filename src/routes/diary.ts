3b60c5009b728cc9f1042da3ec69ae69c90cd908
SnakeBlanket <me@bainble.kr>
2024-11-16 00:00:42 +0900

    feat: use sql grammer LIKE

--- src/routes/diary.ts
+++ src/routes/diary.ts
@@ -45,7 +45,11 @@ router.get(
     let date: string =
       (req.query.date as string) || new Date().toISOString().split('T')[0];
 
-    if (/^\d{8}$/.test(date)) {
+    if (/^\d{6}$/.test(date)) {
+      const year = date.slice(0, 4);
+      const month = date.slice(4, 6);
+      date = `${year}-${month}%`;
+    } else if (/^\d{8}$/.test(date)) {
       const year = date.slice(0, 4);
       const month = date.slice(4, 6);
       const day = date.slice(6, 8);
@@ -68,52 +72,64 @@ router.get(
         });
       }
 
-      if (date.includes('-')) {
-        const [diarys] = await connection.query(
-          'SELECT * FROM diary WHERE user_id = ? AND date = ?',
-          [req.userId, date],
-        );
+      console.log(req.userId, date);
 
-        const diary: Table_Diary = (diarys as Table_Diary[])[0];
+      let query = 'SELECT * FROM diary WHERE user_id = ? AND date LIKE ?';
+      switch (date) {
+        case 'week':
+          query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
+          break;
+        case 'month':
+          query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
+          break;
+        case 'year':
+          query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
+          break;
+      }
 
-        if (diary) {
-          return res.status(200).json({
-            status: 200,
-            title: diary.title,
-            content: diary.content,
-            feel: diary.feel,
-            weather: diary.weather,
-            date: diary.date.replaceAll('-', ''),
-          });
-        } else {
-          return res.status(200).json({
-            status: 200,
-            msg: '검색된 일기가 없습니다',
-          });
-        }
+      const [diarys] = await connection.query(query, [req.userId, date]);
+
+      const diaries: Table_Diary[] = diarys as Table_Diary[];
+
+      if (diaries.length === 0) {
+        return res.status(200).json({
+          status: 200,
+          msg: '검색된 일기가 없습니다',
+        });
+      } else if (diaries.length == 1) {
+        return res.status(200).json({
+          status: 200,
+          title: diaries[0].title,
+          content: diaries[0].content,
+          feel: diaries[0].feel,
+          weather: diaries[0].weather,
+          date: diaries[0].date.replaceAll('-', ''),
+          results: {
+            title: diaries[0].title,
+            content: diaries[0].content,
+            feel: diaries[0].feel,
+            weather: diaries[0].weather,
+            date: diaries[0].date.replaceAll('-', ''),
+          },
+        });
       } else {
-        let query = 'SELECT * FROM diary WHERE user_id = ? AND date LIKE ?';
-        let queryDate = ``;
-        switch (date) {
-          case 'week':
-            query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
-            break;
-          case 'month':
-            query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
-            break;
-          case 'year':
-            query = 'SELECT * FROM diary WHERE user_id = ? AND date >= ?';
-            break;
-        }
-
-        const [diarys] = await connection.query(query, [req.userId, date]);
-
+        return res.status(200).json({
+          status: 200,
+          results: diaries.map((diary) => {
+            return {
+              title: diary.title,
+              content: diary.content,
+              feel: diary.feel,
+              weather: diary.weather,
+              date: diary.date.replaceAll('-', ''),
+            };
+          }),
+        });
       }
     } finally {
       connection.release();
     }
   },
-  authMiddleware,
 );
 
 router.post(
